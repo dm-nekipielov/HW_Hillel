f765726daa51568c0c8b81c9c828ae4928f4c92d
dmnekipelov <dm.nekipelov@gmail.com>
2022-11-01 16:58:12 +0200

    HW30. Multithreading & multiprocessing

--- HW30/dict_keys.py
+++ HW30/dict_keys.py
@@ -4,7 +4,7 @@ class CustomClass:
 
 
 obj_instance_1 = CustomClass(1)
-obj_instance_2 = CustomClass(1)
+obj_instance_2 = CustomClass(2)
 
 test_dict = {
     obj_instance_1: "one",
@@ -14,8 +14,36 @@ test_dict = {
 for key in test_dict:
     if key in [obj_instance_1, obj_instance_2]:
         print("match")
-    else:
-        print("no match")
 
 print(test_dict[obj_instance_1])
 print(test_dict[obj_instance_2])
+
+
+# print(test_dict[CustomClass(2)])  # Так буде помилка
+
+
+class CustomClass_2:
+    def __init__(self, a):
+        self.a = a
+
+    def __eq__(self, other):
+        return self.a is other.a
+
+    def __hash__(self):
+        return hash(self.a)
+
+
+obj_instance = CustomClass_2(1)
+
+test_dict = {
+    CustomClass_2(1): "one",
+    CustomClass_2(2): "two",
+
+}
+
+for key in test_dict:
+    if key == obj_instance:
+        print("match")
+
+print(test_dict[obj_instance])
+print(test_dict[CustomClass_2(2)])
